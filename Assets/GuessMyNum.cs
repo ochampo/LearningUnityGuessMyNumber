8f04fdd7de362f954289a81d866e8af6617f90ad
Daniel Ocampo <daniel.ochampo@gmail.com>
2021-09-17 01:39:56 -0700

    created guess number algorithm

--- Assets/GuessMyNum.cs
+++ Assets/GuessMyNum.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 
 public class GuessMyNum : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max ;
+    int min ;
+    int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
+       
+
+
+    }
+
+
+
+    void StartGame()
+    {
+        int max = 1000;
+        int min = 1;
+        int guess = 500;
         Debug.Log("Give me a number");
         Debug.Log($"Hey {max} and  below");
         Debug.Log($"Hey {min} and  higer");
         Debug.Log("Tell me if your number is 500");
         Debug.Log($"guess {guess}");
+        max = max + 1;
+
+
 
 
     }
+
 
     // Update is called once per frame
     void Update()
@@ -27,23 +44,31 @@ public class GuessMyNum : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow))
         {
             print("you hit up key ");
-            min = guess;
-            guess = (max + min) / 2;
+            NextGuess();
         }
 
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             print("you hit down key");
-            max = guess;
-            guess = (max + min) / 2;
+            NextGuess();
+
+
         }
        else if (Input.GetKey(KeyCode.Return))
         {
             print("you hit Enter key");
-            print("you hit Enter key");
-            print("you hit Enter key");
-            print("you hit Enter key");
+            
         }
 
     }
+
+    void NextGuess()
+    {
+        max = guess;
+        guess = (max + min) / 2;
+    }
+
+
+
+
 }
